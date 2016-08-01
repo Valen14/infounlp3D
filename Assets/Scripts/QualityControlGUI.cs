092c163b9da129b29383dc1cf0d0a0ef5deb3845
Federico <federicocristina@gmail.com>
2016-08-01 16:56:11 -0300

    Far Clipping Plane opcional desde la GUI a fin de mejorar performance en dispositivos móviles

---
+++ Assets/Scripts/QualityControlGUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QualityControlGUI : MonoBehaviour {
+
+	public Camera animCamera;
+	public Camera roamCamera;
+
+	protected static float MAX_FAR_CLIPPING = 200f;
+	protected static float MIN_FAR_CLIPPING = 10f;
+
+	protected float farClipping = MAX_FAR_CLIPPING;
+
+	// Use this for initialization
+	void Start () {
+		#if MOBILE_INPUT	
+		farClipping = 60;
+		#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		GUI.Label (new Rect (10, Screen.height - 30, 50, 25), "Dist:");
+		farClipping = GUI.HorizontalSlider (new Rect (50, Screen.height - 25, 120, 25), farClipping, MIN_FAR_CLIPPING, MAX_FAR_CLIPPING);
+		animCamera.farClipPlane = farClipping;
+		roamCamera.farClipPlane = farClipping;
+	}
+}
